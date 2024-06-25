7988cd9ba6ebddd9ec0d137f8693549ff7484cb6
Ria Shroff <ria.shroff.8@gmail.com>
2024-06-24 19:42:50 -0500

    hello!

---
+++ week1.py
@@ -0,0 +1,29 @@
+#Codecademy
+print("Hello world!")
+
+#futurecoder
+print(1+2)
+print(1/2)
+print(2/1)
+print(1*2)
+print((1+2)*(2/1*2/1/2))
+
+print("Hello" + " World!")
+print("Hello" + " " + "World!")
+
+word = "Hello"
+your_name = "Ria"
+print(word + " " + your_name)
+sentence = word + " " + your_name
+print(sentence)
+
+name = "World"
+line = ""
+for char in name:
+    line = line + char
+    print(line)
+
+for char in name:
+    line += "-"
+print(name)
+print(line)
