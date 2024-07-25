794e15f40aff4329adc974450db40a1ecfa8761e
Ria Shroff <ria.shroff.8@gmail.com>
2024-07-24 20:46:31 -0500

    updated

---
+++ week4/README.md
@@ -0,0 +1,5 @@
+# What I did in Week 4
+## Python
+### I continued using futurecoder to learn more python skills
+### On 07.24.2024, I coded a simple number guessing program.
+#### This app tested my knowledge of python so far and the amount of time I need to code a program.
--- week4/practice.py
+++ week4/practice.py
@@ -10,3 +10,22 @@ for c in name:
     new_name += c
     
 print(new_name)
+# ---------------------------------------------------------------------------------------------------------
+import random
+numbers = ['1','2','3','4','5','6','7','8','9','10']
+def game():
+    userSelection = input("Welcome to Ria's Guessing Game! Guess a number between 1 and 10:     ")
+    computerSelection = random.choice(numbers)
+    # using items in for loop causes it to end after 10 tries
+    for items in numbers:
+        if computerSelection == userSelection:
+            print('')
+            print(f'Correct! The number was {computerSelection}')
+            print('')
+            break
+        else:
+            print('')
+            print("Nope, that's not it. Try again!")
+            userSelection = input("Enter a number between 1 and 10:   ")
+
+game()
